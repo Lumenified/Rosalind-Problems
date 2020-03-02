7366f0e43f5c29027a3274c0bd2cdf32ca28ef08
Lumenifiedgit <tahsinsaid.turan@gazi.edu.tr>
2020-03-02 22:42:39 +0300

    Rosalind Problems

--- tuts3.py
+++ tuts3.py
@@ -5,7 +5,6 @@ def ReverseComplimentaryDNA():
     A small tweak (reading and modifying the file).
     """
     with open("rosalind_revc.txt") as p:
-        my_seq = p.readlines()
-        seq = Seq(my_seq[0])
+        seq = Seq(p.readlines()[0])
         return seq.reverse_complement()
 print(ReverseComplimentaryDNA())
