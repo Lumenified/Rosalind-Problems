1c05c1afb674c15a5f3a85bb7946d592a8fd7515
Lumenifiedgit <tahsinsaid.turan@gazi.edu.tr>
2020-03-04 12:08:44 +0300

    Rosalind Problems

---
+++ tuts6.py
@@ -0,0 +1,11 @@
+with open("rosalind_hamm.txt") as p:
+    my_whole_seq = str(p.read()).replace("\n", "")
+
+def Hamming_Calculation(seq):
+    counter = 0
+    for i in range(int(len(seq)/2)):
+        if seq[i]!=seq[(int(len(seq)/2))+i]:
+            counter += 1
+    return counter
+
+print(Hamming_Calculation(my_whole_seq))
